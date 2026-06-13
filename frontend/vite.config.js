bda98457726c61035751061579d4f59e547cfe29
Emilio Calderon <ecalderon-patino@csub.edu>
2026-06-12 21:04:13 -0700

    Add Lab D React frontend scaffold

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/-cmps3350-teamB-The-San-Joaquin-Kit-Fox-project/lab-d-gael/',
+  base: '/teamB/lab-d-Emilio/',
   plugins: [react()],
 })
