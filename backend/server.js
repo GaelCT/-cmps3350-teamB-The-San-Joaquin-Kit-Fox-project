216236543e6ad446f9242c3314c596a7d00aad3d
amiranda1004 <ashley.miran831@gmail.com>
2026-06-28 11:33:46 -0700

    Add CORS middleware

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { pool } from "./db.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || "127.0.0.1";
 
 app.use(express.json());
+app.use(cors());
 
 function isValidId(value) {
   const id = Number(value);
